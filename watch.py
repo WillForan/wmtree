5c28cbebc105076c265ae227fd4c4d05d7e7f5cf
WillForan <willforan@gmail.com>
2018-08-06 16:42:20 -0400

    added pid, update broken

--- watch.py
+++ watch.py
@@ -1,26 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Based on code by Stephan Sokolow
 # Source: https://gist.github.com/ssokolow/e7c9aae63fb7973e4d64cff969a78ae8
 """python-xlib example which reacts to changing the active window/title.
-Requires:
-- Python
-- python-xlib
-Tested with Python 2.x because my Kubuntu 14.04 doesn't come with python-xlib
-for Python 3.x.
-Design:
--------
-Any modern window manager that isn't horrendously broken maintains an X11
-property on the root window named _NET_ACTIVE_WINDOW.
-Any modern application toolkit presents the window title via a property
-named _NET_WM_NAME.
-This listens for changes to both of them and then hides duplicate events
-so it only reacts to title changes once.
+ - requires python-xlib.
+
+intend to give a clickable display of x11 clients as a tree.
+potentially a replacement for tree style tabs outside of firefox
+e.g. use with uzbl or vimb
+see
+  pstree -sp $(xprop _NET_WM_PID |cut -f2 -d=)
+  xwininfo -root -tree
+  xlsclients
 """
 
 from contextlib import contextmanager
 import Xlib
 import Xlib.display
+import psutil
 
 # Connect to the X server and get the root window
 disp = Xlib.display.Display()
@@ -31,7 +28,10 @@ NET_ACTIVE_WINDOW = disp.intern_atom('_NET_ACTIVE_WINDOW')
 NET_WM_NAME = disp.intern_atom('_NET_WM_NAME')  # UTF-8
 WM_NAME = disp.intern_atom('WM_NAME')           # Legacy encoding
 
-last_seen = {'xid': None, 'title': None}
+WM_PID = disp.intern_atom('_NET_WM_PID')
+
+last_seen = {'xid': None, 'title': None, 'pid': None}
+prev_seen = last_seen
 
 
 @contextmanager
@@ -67,7 +67,7 @@ def get_active_window():
 
 def _get_window_name_inner(win_obj):
     """Simplify dealing with _NET_WM_NAME (UTF-8) vs. WM_NAME (legacy)"""
-    for atom in (NET_WM_NAME, WM_NAME):
+    for atom in (NET_WM_NAME, WM_NAME, WM_PID):
         try:
             window_name = win_obj.get_full_property(atom, 0)
         except UnicodeDecodeError:  # Apparently a Debian distro package bug
@@ -88,16 +88,24 @@ def _get_window_name_inner(win_obj):
 
 def get_window_name(win_id):
     """Look up the window name for a given X11 window ID"""
-    if not win_id:
-        last_seen['title'] = "<no window id>"
-        return last_seen['title']
+    global prev_seen, last_seen
+    win_title = None
+
+    # never hit
+    # if not win_id:
+    #     last_seen['title'] = "<no window id>"
+    #     return last_seen['title']
 
     title_changed = False
     with window_obj(win_id) as wobj:
         if wobj:
             win_title = _get_window_name_inner(wobj)
             title_changed = (win_title != last_seen['title'])
-            last_seen['title'] = win_title
+
+    if title_changed:
+        prev_seen = last_seen
+        last_seen['title'] = win_title
+        last_seen['pid'] = int(wobj.get_full_property(WM_PID, 0).value[0])
 
     return last_seen['title'], title_changed
 
@@ -109,17 +117,19 @@ def handle_xevent(event):
 
     changed = False
     if event.atom == NET_ACTIVE_WINDOW:
-        if get_active_window()[1]:
+        if get_active_window()[1]:   # if window focus has changed
             changed = changed or get_window_name(last_seen['xid'])[1]
     elif event.atom in (NET_WM_NAME, WM_NAME):
         changed = changed or get_window_name(last_seen['xid'])[1]
 
     if changed:
-        handle_change(last_seen)
+        handle_change(last_seen, prev_seen)
 
 
-def handle_change(new_state):
+def handle_change(new_state, prev_state):
     """Replace this with whatever you want to actually do"""
+    closed = not psutil.pid_exists(prev_state['pid'])  # close or change focus
+    print(closed)
     print(new_state)
 
 
@@ -129,7 +139,7 @@ if __name__ == '__main__':
 
     # Prime last_seen with whatever window was active when we started this
     get_window_name(get_active_window()[0])
-    handle_change(last_seen)
+    handle_change(last_seen, prev_seen)
 
     while True:  # next_event() sleeps until we get an event
         handle_xevent(disp.next_event())
